1afbd4550256d78d0f7c4437cb5722bb1caf4ddd
Alexander Kern <alexander.t.kern@gmail.com>
2023-08-30 16:05:05 -0700

    TextInput testing

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Linking} from 'react-native';
+import { StyleSheet, Text, View, Button, Linking, TextInput} from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
 
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("Alex");
+
+  const updateName = (newName) => {
+    setName(newName);
+  }
 
   const incrementCounter = () => {
     setCounter(counter + 1);
@@ -21,12 +26,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text> {counter}</Text>
+      <Text> {name}'s counter: {counter}</Text>
+
       <Button onPress={incrementCounter} style={styles.button} title='Increment' ></Button>
       <Button onPress={decrementCounter} style={styles.button} title='Decrement' ></Button>
       <Button onPress={halfCounter} style={styles.button} title='Half'></Button>
       <Button onPress={resetCounter} style={styles.button} title='Reset'></Button>
-      <Button onPress={() => Linking.openURL('https://youtube.com/gymnast86')} style={styles.button} title='Youtube'></Button>
+      <TextInput onChangeText={text => updateName} placeholder='Update Name' />
+      {/* <Button title='Youtube' onPress={() => Linking.openURL('https://youtube.com/gymnast86')} style={styles.button} ></Button> */}
 
 
       <StatusBar style="auto" />
